856da7cc736d0028c9b29c08dfc503c4ce54e3ce
tanbiralam <itzmetanbir@gmail.com>
2025-06-02 22:44:41 +0530

    next config updated

--- components/InterviewCard.tsx
+++ components/InterviewCard.tsx
@@ -5,8 +5,9 @@ import Image from "next/image";
 import { Button } from "./ui/button";
 import Link from "next/link";
 import DisplayTechIcons from "./DisplayTechIcons";
+import { getFeedbackByInterviewId } from "@/lib/actions/general.action";
 
-const InterviewCard = ({
+const InterviewCard = async ({
   id,
   userId,
   role,
@@ -14,7 +15,10 @@ const InterviewCard = ({
   techstack,
   createdAt,
 }: InterviewCardProps) => {
-  const feedback = null as Feedback | null;
+  const feedback =
+    userId && id
+      ? await getFeedbackByInterviewId({ interviewId: id, userId })
+      : null;
   const normalizedType = /mix/gi.test(type) ? "Mixed" : type;
   const formattedDate = dayjs(
     feedback?.createdAt || createdAt || Date.now()
@@ -50,17 +54,16 @@ const InterviewCard = ({
           {feedback?.finalAssessment ||
             "You haven't taken the interview yet. Take it now to improve your skills."}
         </p>
-      </div>
-
-      <div className="flex flex-row justify-between">
-        <DisplayTechIcons techStack={techstack} />
-        <Button className="btn-primary">
-          <Link
-            href={feedback ? `/interview/${id}/feedback` : `/interview/${id}`}
-          >
-            {feedback ? "Check Feedback" : "View"}
-          </Link>
-        </Button>
+        <div className="flex flex-row justify-between">
+          <DisplayTechIcons techStack={techstack} />
+          <Button className="btn-primary">
+            <Link
+              href={feedback ? `/interview/${id}/feedback` : `/interview/${id}`}
+            >
+              {feedback ? "Check Feedback" : "View"}
+            </Link>
+          </Button>
+        </div>
       </div>
     </div>
   );
